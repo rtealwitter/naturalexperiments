fcff482c4bd11716528193de63aaac528b7e2c0c
R. Teal Witter <rtealwitter@gmail.com>
2024-05-22 16:58:15 -0400

    speed up test with faster estimator

--- naturalexperiments/test.py
+++ naturalexperiments/test.py
@@ -5,7 +5,7 @@ from estimators import methods
 def test():
     X, y, z = dataloaders['RORCO']()
     p = estimate_propensity(X, z)
-    estimate = methods['Double-Double'](X, y, z, p, train)
+    estimate = methods['Regression Discontinuity'](X, y, z, p, train)
 
 if __name__ == '__main__':
     test()
