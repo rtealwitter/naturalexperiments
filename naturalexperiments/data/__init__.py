5d916e68836ea6e5432def8ee0af703318f40e6a
R. Teal Witter <rtealwitter@gmail.com>
2024-05-24 10:59:50 -0400

    rorco=semisynthetic

--- naturalexperiments/data/__init__.py
+++ naturalexperiments/data/__init__.py
@@ -2,14 +2,15 @@ from .acic import load_acic
 from .ihdp import load_ihdp
 from .jobs import load_jobs
 from .news import load_news
-from .rorco import load_rorco
+from .rorco import load_rorco, load_rorco_real
 from .twins import load_twins
 
 dataloaders = {
+    'RORCO Real' : load_rorco_real,
+    'RORCO' : load_rorco,
     'ACIC' : load_acic,
     'IHDP' : load_ihdp,
     'JOBS' : load_jobs,
     'NEWS' : load_news,
-    'RORCO' : load_rorco,
     'TWINS' : load_twins,
 }
--- naturalexperiments/data/rorco/__init__.py
+++ naturalexperiments/data/rorco/__init__.py
@@ -1,11 +1,11 @@
 import numpy as np
 import pandas as pd
-import pickle as pkl
 import sklearn.preprocessing
+from ...utils import build_synthetic_outcomes
 import os
 import requests
 
-def load_rorco():
+def load_rorco_real():
     filename = __file__.replace('__init__.py', 'rorco_data.csv')
 
     if not os.path.exists(filename):
@@ -41,3 +41,30 @@ def load_rorco():
 
     return X, y, z
 
+def load_rorco():
+    filename = __file__.replace('__init__.py', 'rorco_data.csv')
+
+    if not os.path.exists(filename):
+        print('Downloading RORCO data...')
+        # Download the data 
+        url = 'https://raw.githubusercontent.com/rtealwitter/naturalexperiments/main/naturalexperiments/data/rorco/rorco_data.csv'
+
+        r = requests.get(url)
+        open(filename, 'wb').write(r.content)
+
+    data = pd.read_csv(filename)
+
+    outcome_variable = 'Mean Scale Score'
+    treatment_variable = 'is_RORCO'
+
+    X = data.drop(columns = [treatment_variable, outcome_variable, 'num_from_RORCO'])
+
+    X = X.select_dtypes(include = ['int16', 'int32', 'int64', 'float16', 'float32', 'float64'])
+    X.replace(np.nan, 0, inplace=True)
+
+    X = sklearn.preprocessing.StandardScaler().fit(X.values).transform(X.values)
+
+    X, y, p = build_synthetic_outcomes(X)
+    uniform = np.random.random_sample(X.shape[0])
+    z = (uniform < p).astype(int)
+    return X, y, z
