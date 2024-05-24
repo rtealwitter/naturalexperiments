16089c16e486afa664532fd60b9216031bd490aa
R. Teal Witter <rtealwitter@gmail.com>
2024-05-24 11:00:02 -0400

    new version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="naturalexperiments",
-    version="0.0.7",
+    version="0.0.8",
     author="R. Teal Witter",
     author_email="rtealwitter@gmail.com",
     description="Estimators and datasets for treatment effect estimation in natural experiments.",
